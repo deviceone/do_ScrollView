5a4fabdfed75ba67cac5ca21b143c2d73b673876
zhangrj <416859389@qq.com>
2015-03-23 10:40:22 +0800

    1.HScollView的isShowbar方法原来设置的是VerticalScollBarEnabled属性，更正为HorizontalScrollBarEnabled属性 2.相应的在propertyChanged函数里将变量名verticalScrollBarEnabled改成scrollBarEnabled 3.在两个内部类HScrollView和VScrollView的构造函数里添加this.setHorizontalScrollBarEnabled(false)和this.setVerticalScrollBarEnabled(false);语句，从而匹配设计文档的初始值设定

---
+++ src/android/doExt_Do_ScrollView/src/extimplement/do_ScrollView_Model.java
@@ -0,0 +1,15 @@
+package extimplement;
+
+import extdefine.do_ScrollView_MAbstract;
+
+/**
+ * 自定义扩展组件Model实现，继承Do_ScrollView_MAbstract抽象类；
+ *
+ */
+public class do_ScrollView_Model extends do_ScrollView_MAbstract {
+
+	public do_ScrollView_Model() throws Exception {
+		super();
+	}
+	
+}
